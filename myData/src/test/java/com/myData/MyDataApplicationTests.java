7bbdd94a2d8bc7d507e7f3035d4c643593ada908
falterguy <yeong4573@nate,com>
2024-08-29 22:12:43 +0900

    a

---
+++ myData/src/test/java/com/myData/MyDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
